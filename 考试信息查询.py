4e9d9d5b5870c77fb842d6a6c3526da4d95bdf7c
Ning <33782391+Lipang@users.noreply.github.com>
2018-10-08 11:05:14 +0800

    Update 考试信息查询.py

--- 考试信息查询.py
+++ 考试信息查询.py
@@ -1,4 +1,3 @@
-from 课表查询 import KbCx
 import xlwt
 import requests
 import os
@@ -6,12 +5,11 @@ import os
 
 class Ksxxcx:
 
-    def __init__(self):
-        # url记得改
-        #self.username = username
-        self.url = 'http://jwgl.cug.edu.cn/jwglxt/kwgl/kscx_cxXsksxxIndex.html?doType=query&gnmkdm=N358105&su='
+    def __init__(self, ses):
+        self.url = 'http://jwgl.cug.edu.cn/jwglxt/kwgl/kscx_cxXsksxxIndex.html?doType=query&gnmkdm=N358105'
+        self.ses = ses
 
-    def ksxxcx(self, headers):
+    def ksxxcx(self):
         if os.path.exists('./考试信息查询结果'):
             pass
         else:
@@ -19,13 +17,13 @@ class Ksxxcx:
         print('目前官网只能查看当前学年的考试信息！')
         xnm = input('请输入要查询的学年(格式:2017-2018):')
         xqm = input('请输入要查询的学期(格式:2):')
-        data = data1.get_cxdata(xnm, xqm)
+        data = self.get_cxdata(xnm, xqm)
         data['queryModel.showCount'] = 30
-        html = requests.post(self.url, headers=headers, data=data)
+        html = self.ses.post(self.url, data=data)
         f = xlwt.Workbook()
         sheet = f.add_sheet('sheet1')
-        bt = ['学年', '学期', '学号', '姓名', '性别', '班级', '课程代码', '课程名称', '重修标记', '自修标记', '考试时间', '考试地点', '考试校区', '考试座位号', '学院',
-              '专业']
+        bt = ['学年', '学期', '学号', '姓名', '性别', '班级', '课程代码', '课程名称', '重修标记', '自修标记', '考试时间', '考试地点',
+              '考试校区', '考试座位号', '学院', '专业']
         for a in range(0, len(bt)):
             sheet.write(0, a, bt[a])
         if html.json()['totalCount'] != 0:
@@ -54,19 +52,21 @@ class Ksxxcx:
         else:
             print('没有您所查询的信息')
 
+    def get_cxdata(self, xnm, xqm):
+
+        xq = {
+            '1': 3,
+            '2': 12,
+            '3': 16
+        }
+        data = {
+            'xnm': xnm[0:4],
+            'xqm': xq[xqm]
+        }
+        return data
+
     def save2excel(self, data, row, sheet):
 
         for i in range(0, len(data)):
             sheet.write(row + 1, i, data[i])
 
-data1 = KbCx()
-if __name__ == '__main__':
-    header = {
-        'Cookie': 'JSESSIONID = 1AB8DF05C6AC733486B291BD94753C6A',
-        'Host': 'jwgl.cug.edu.cn',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
-    }
-
-    while True:
-        exam_seg = Ksxxcx()
-        exam_seg.ksxxcx(header)
