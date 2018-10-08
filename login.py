573bdae9c73f45d784ebecf0f45d02973eeb5c2b
Ning <33782391+Lipang@users.noreply.github.com>
2018-10-08 11:02:43 +0800

    Update login.py
    修改登录方式，由以前的selenium 改为现在的命令行登录

--- login.py
+++ login.py
@@ -1,78 +1,78 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from 空教室查询 import KxCdCx
 from 课表查询 import KbCx
 from 考试信息查询 import Ksxxcx
 from 个人成绩查询 import StuScore
-import time
 import re
+import execjs
+import requests
 
 
-class Login:
-    def __init__(self):
-        pass
+def login(username, passwd):
+    url1 = 'http://sfrz.cug.edu.cn/tpass/login?service=http%3A%2F%2Fxyfw.cug.edu.cn%2Ftp_up%2Fview%3Fm%3Dup#act=portal/viewhome'
+    url2 = 'http://sfrz.cug.edu.cn/tpass/login?service=http%3A%2F%2Fxyfw.cug.edu.cn%2Ftp_up%2F'
+    header1 = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
+    }
+    header2 = {
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
+        'Referer': 'http://sfrz.cug.edu.cn/tpass/login?service=http%3A%2F%2Fxyfw.cug.edu.cn%2Ftp_up%2Fview%3Fm%3Dup',
+        'Origin': 'http://sfrz.cug.edu.cn',
+        'Host': 'sfrz.cug.edu.cn',
+    }
+    ses = requests.session()
+    res = ses.get(url=url1, headers=header1)
+    res.encoding = 'utf-8'
+    text = res.text
+    parrtern1 = re.compile(r'<input type="hidden" id="lt" name="lt" value="(.*?)" />')
+    parrtern2 = re.compile(r'<input type="hidden" name="execution" value="(.*?)" />')
 
-    def login_cug(self):
-        username = input('请输入你的账号:')
-        password = input('请输入你的密码:')
-        print('登陆中，请稍等。。。')
-        brower = webdriver.Firefox()
-        try:
-            brower.get('http://sfrz.cug.edu.cn/tpass/login?service=http%3A%2F%2Fxyfw.cug.edu.cn%2Ftp_up%2Fview%3Fm%3Dup')
-            inputun = brower.find_element_by_id('un')
-            inputun.send_keys(username)
-            time.sleep(1)
-            inputun = brower.find_element_by_id('pd')
-            inputun.send_keys(password)
-            inputun.send_keys(Keys.ENTER)
-            wait = WebDriverWait(brower, 20)
-            wait.until(EC.element_to_be_clickable((By.ID, 'app_a')))
-            html = brower.page_source
-            appurl = re.findall('appurl="(.*?)"', html, re.S)
-            appurl = appurl[0]
-            brower.get(appurl)
-            cookie = brower.get_cookies()
+    lt = re.search(parrtern1, text).group(1)
+    execution = re.search(parrtern2, text).group(1)
+    with open('dec.js', 'r') as f:
+        keys = execjs.compile(f.read()).call('strEnc', username + passwd + lt, '1', '2', '3')
+    data = {
+        'rsa': keys,
+        'ul': str(len(username)),
+        'pl': str(len(passwd)),
+        'lt': lt,
+        'execution': execution,
+        '_eventId': 'submit',
+    }
 
-        finally:
-            brower.close()
-        cookie = cookie[0]['name'] + '=' + cookie[0]['value']
-        return cookie
+    r = ses.post(url2, headers=header2, data=data)
+    r = ses.get('http://202.114.207.137:80/ssoserver/login?ywxt=jw')
+    return ses
 
 
 if __name__ == '__main__':
-    Me = Login()
     print('欢迎使用本产品，输入0退出。')
+    # 用来控制退出的数字
     zong_code = 1
     while input() != '0' and zong_code != 0:
-        cookie = Me.login_cug()
+        username = input('用户名: ')
+        passwd = input('密码: ')
+        ses = login(username, passwd)
         print('登录成功，请开始你的表演！')
-        headers = {
-            'Cookie': cookie,
-            'Host': 'jwgl.cug.edu.cn',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
-        }
-        ClassScdSearch = KbCx()  # 课表查询
-        ClassRoomSearch = KxCdCx()  # 空闲教室查询
-        ExamMsgSearch = Ksxxcx()  # 考试信息查询
-        StuScore = StuScore(headers)  # 个人成绩查询
+        ClassScdSearch = KbCx(ses)  # 课表查询
+        ClassRoomSearch = KxCdCx(ses)  # 空闲教室查询
+        ExamMsgSearch = Ksxxcx(ses)  # 考试信息查询
+        StuScore = StuScore(ses)  # 个人成绩查询
         quit_code = 1
         while quit_code != 0:
-            print('\n'+'*******************************')
+            print('\n' + '*' * 20)
             print('1.查询个人课表', '2.查询空闲教室', '3.查询考试信息', '4.查询个人成绩', '5.退出', sep='\n')
-            print('*******************************'+'\n')
+            print('*' * 20 + '\n')
             user_input = int(input('请输入:'))
             print('\n')
             if user_input == 1:
-                ClassScdSearch.get_grkbxx(headers)
+                ClassScdSearch.get_grkbxx()
             elif user_input == 2:
-                ClassRoomSearch.get_cdxx(headers)
+                ClassRoomSearch.get_cdxx()
             elif user_input == 3:
-                ExamMsgSearch.ksxxcx(headers)
+                ExamMsgSearch.ksxxcx()
             elif user_input == 4:
-                 StuScore.stu_score()
+                StuScore.stu_score()
             elif user_input == 5:
                 print('欢迎下次使用，再见(*╹▽╹*)')
                 quit_code = 0
