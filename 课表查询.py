401ca394a8925a1cdef338b36b6f95b412388195
Ning <33782391+Lipang@users.noreply.github.com>
2018-10-08 11:05:36 +0800

    Update 课表查询.py

--- 课表查询.py
+++ 课表查询.py
@@ -1,12 +1,13 @@
 from prettytable import PrettyTable
-import requests
 import os
 
-class KbCx:
-    def __init__(self):
-        self.url = 'http://jwgl.cug.edu.cn/jwglxt/kbcx/xskbcx_cxXsKb.html?gnmkdm=N2151'
 
-    def get_grkbxx(self, headers):
+class KbCx:
+    def __init__(self, ses):
+        self.url = 'http://jwgl.cug.edu.cn/jwglxt/kbcx/xskbcx_cxXsKb.html?gnmkdm=N2151'
+        self.ses = ses
+
+    def get_grkbxx(self):
         if os.path.exists('./学生个人课表'):
             pass
         else:
@@ -14,24 +15,25 @@ class KbCx:
         xnm = input('请输入要查询的学年(格式:2017-2018):')
         xqm = input('请输入要查询的学期(格式:2):')
         data = self.get_cxdata(xnm, xqm)
-        html = requests.post(self.url, headers=headers, data=data).json()
+        html = self.ses.post(self.url, data=data).json()
         kblists = html['kbList']  # 课程列表
         sjlists = html['sjkList']  # 实践课列表
         xsxm = html['xsxx']['XM']
 
         pt1 = PrettyTable(['星期', '节次', '周次', '课程名称', '任课老师', '上课地点'])
         for i in range(len(kblists)):
-            pt1.add_row([kblists[i]['xqjmc'], kblists[i]['jc'], kblists[i]['zcd'], kblists[i]['kcmc'], kblists[i]['xm'], kblists[i]['cdmc']])
+            pt1.add_row([kblists[i]['xqjmc'], kblists[i]['jc'], kblists[i]['zcd'], kblists[i]['kcmc'], kblists[i]['xm'],
+                         kblists[i]['cdmc']])
         with open('./学生个人课表/{}学年第{}学期学生个人课表.txt'.format(xnm, xqm), 'a', encoding='utf-8') as f:
             f.write('平时课程表')
-            f.write('\n'+str(pt1))
+            f.write('\n' + str(pt1))
 
         pt2 = PrettyTable(['实习时间', '实习课名称', '指导老师'])
         for n in range(len(sjlists)):
-            pt2.add_row([sjlists[n]['qsz']+'-'+sjlists[n]['zzz']+'周', sjlists[n]['kcmc'], sjlists[n]['xm']])
+            pt2.add_row([sjlists[n]['qsz'] + '-' + sjlists[n]['zzz'] + '周', sjlists[n]['kcmc'], sjlists[n]['xm']])
         with open('./学生个人课表/{}学年第{}学期学生个人课表.txt'.format(xnm, xqm), 'a', encoding='utf-8') as f:
-            f.write('\n'*2+'实习课程表')
-            f.write('\n'+str(pt2))
+            f.write('\n' * 2 + '实习课程表')
+            f.write('\n' + str(pt2))
         print('查询成功')
 
     def get_cxdata(self, xnm, xqm):
@@ -47,11 +49,3 @@ class KbCx:
         }
         return data
 
-
-if __name__ == '__main__':
-    headers = {
-        'Cookie': 'JSESSIONID=1AB8DF05C6AC733486B291BD94753C6A'
-    }
-    while True:
-        a = KbCx()
-        a.get_grkbxx(headers)
